693d1767a4654bd0713acb3311ed34868f588751
Sunil Bagul <sunilbagul@users.noreply.github.com>
2021-07-07 20:19:38 +0530

    Update myfirstec2.tf

--- myfirstec2.tf
+++ myfirstec2.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "PUT-YOUR-ACCESS-KEY-HERE"
-  secret_key = "PUT-YOUR-SECRET-KEY-HERE"
+  access_key = "AKIAVJUNINJXNIHDOBWF"
+  secret_key = "TwQnfSAlRaMipqA+lahylpzod2ffUxs+QyJZd5nN"
 }
 
 resource "aws_instance" "myec2" {
-   ami = "ami-082b5a644766e0e6f"
+   ami = "ami-0721c9af7b9b75114"
    instance_type = "t2.micro"
 }
